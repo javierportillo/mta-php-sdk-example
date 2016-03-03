825c91ee4d484de579e04a702edf14d1e71302c0
Javier Portillo <javimarmota@gmail.com>
2016-03-03 00:13:00 -0600

    Add comments to functionCalledFromPHP

--- lua/functions.lua
+++ lua/functions.lua
@@ -1,4 +1,7 @@
-function functionCalledFromPHP(value)
-    outputDebugString(value)
+-- This exported fuction is called from website.php
+-- the 'message' argument here is passed from the second parameter of the 'call'
+-- method. And the returned value goes back to PHP.
+function functionCalledFromPHP(message)
+    outputDebugString(message)
     return getNetworkStats()
 end
