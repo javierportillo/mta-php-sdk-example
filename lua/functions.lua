0ddc226a37fa710632e8d8681ce4ca5b6e918586
Javier Portillo <javimarmota@gmail.com>
2016-03-03 21:28:39 -0600

    Add comments for lua to php to lua functions

--- lua/functions.lua
+++ lua/functions.lua
@@ -1,18 +1,35 @@
 -- This exported fuction is called from website.php
 -- the 'message' argument here is passed from the second parameter of the 'call'
--- method. And the returned value goes back to PHP.
+-- method in PHP. And the returned value goes back to PHP.
 function functionCalledFromPHP(message)
     outputDebugString(message)
     return getNetworkStats()
 end
 
+-- The example below shows how you can use the PHP SDK to send data over to PHP using
+-- the callRemote function. This function is asynchronous, wich is just a fancy word
+-- for saying that you can't just store the returned values in a variable, instead you
+-- have to create another function for callRemote to call back whenever the results are
+-- delivered from PHP back to MTA.
 function getAnimalDNASector(player, cmd, animal)
-    callRemote('http://127.0.0.1:8000/animal_dna.php',animalDNAResult, animal)
+    -- First argument is the URL where your php file is located.
+    -- Second argument is the callback function that receives the data from PHP.
+    -- The rest are a list of optional arguments that you may want to send.
+    callRemote('http://127.0.0.1:8000/animal_dna.php', animalDNAResult, animal)
 end
 
+-- Once the PHP script finishes, the callback function is called. The mta::doReturn();
+-- method is used to pass arguments back to this function. If no parameters are declared
+-- in the doReturn method, you still have to declare two arguments in the callback function
+-- that you can use when errors occur to catch the error message.
+-- If there's an error, the first argument will contain the string "ERROR". The second
+-- argument contains an integer with the error code, for example: 404 (if the requested
+-- file was not found).
 function animalDNAResult(animal, dna_sequence)
     outputDebugString('First sector of DNA for ' .. animal)
     outputDebugString(dna_sequence)
 end
 
+-- For this example I've used PHP to get the first sector of the DNA sequence of a couple
+-- animals. For the time being, you can only get the DNA sequence for alpaca and dolphin.
 addCommandHandler('getdna', getAnimalDNASector)
--- php/animal_dna.php
+++ php/animal_dna.php
@@ -1,8 +1,29 @@
 <?php
 include_once('mtaphpsdk_0.4/mta_sdk.php');
 
-$animal = mta::getInput()[0];
+/*
+ * This page is not meant to be loaded on a web browser. You have to be cautious
+ * of how this code would behave if it gets loaded on a browser. Preferably,
+ * you would use this file exclusively to handle conections comming from MTA; It
+ * is adviced in the PHP SDK wiki that you should not output any data to the
+ * screen, but you can be as creative as you want with your implementation.
+ */
 
+/*
+ * getInput colects all the argumets passed from the callRemote function and
+ * stores them in an array.
+ */
+$remote_input = mta::getInput();
+
+/*
+ * The next block of code is used to select a database depending on what animal
+ * you selected in the /getdna command. If you are wondering about why did I
+ * choose to make this animal dna example, it's because that was the only publicly
+ * accesible database I could find on the internet. It was actualy very interesting
+ * to find out that there's a public mysql database full of dna sequencing related stuff.
+ */
+
+$animal = $remote_input[0];
 if ($animal == 'alpaca') {
     $db_name = 'vicugna_pacos_core_83_1';
 } elseif ($animal == 'dolphin') {
@@ -11,10 +32,19 @@ if ($animal == 'alpaca') {
     mta::doReturn($animal, 'Catastrophic Failure: Critical system files have been compromised. Please restart your computer.');
     exit(1);
 }
-
+/*
+ * The code below connects to the database. I mean, you could have just connected
+ * directly from MTA using the SQL functions... but whatever, I guess. :P
+ * This is just an example, you can do whatever you want with PHP and then just
+ * pass the result back to MTA.
+ */
 $database_connection = new mysqli('ensembldb.ensembl.org', 'anonymous', '', $db_name);
 $query_result = $database_connection->query('SELECT sequence FROM dna WHERE seq_region_id = 1');
 $dna_sequence = $query_result->fetch_row()[0];
 
+/*
+ * doReturn takes any number of parameters and sends them back to MTA, then it
+ * gets handled by the callback function declared in callRemote.
+ */
 mta::doReturn($animal, $dna_sequence);
 ?>
